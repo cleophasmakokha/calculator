43465cff54526e11acd3c2be7b15edb6b4710e44
cleophas <makokhacleophas@gmail.com>
2020-05-02 22:58:42 +0300

    Adds a division function to the script.

--- js/script.js
+++ js/script.js
@@ -24,3 +24,12 @@ var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
 var result = multiply(number1, number2);
 alert(result);
+
+var division= function(number1, number2) {
+return number1 / number2;
+};
+
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var result = division(number1, number2);
+alert(result);
