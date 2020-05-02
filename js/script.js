912525ef9525c045e06f998d4808b308e38a546e
cleophas <makokhacleophas@gmail.com>
2020-05-02 22:54:54 +0300

    Adds a multiplication function to the script.

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ var number1 = parseInt(prompt("Enter a number:"));
 var number2 = parseInt(prompt("Enter another number:"));
 var result = add(number1, number2);
 alert(result);
+
 var subtract= function(number1, number2) {
 return number1 - number2;
 };
